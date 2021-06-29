b75d51af0b3ce56cc6066e4f9a832d90319a1450
Guillermo Sesena <g.sesena.pascacio@accenture.com>
2021-06-29 15:31:43 -0700

    5/38 My first class was created

---
+++ src/Doctor.java
@@ -0,0 +1,15 @@
+public class Doctor {
+    //Attributes
+    int id;
+    String name;
+    String speciality;
+
+    //Methods
+    /**
+     * Description: Print the name of the doctor.
+     * */
+    public void showName()
+    {
+        System.out.println(name);
+    }
+}
