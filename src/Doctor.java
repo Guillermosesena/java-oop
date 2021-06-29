1994973aa4fa6f4e910bf1cbb69b75f6352bba58
Guillermo Sesena <g.sesena.pascacio@accenture.com>
2021-06-29 15:53:26 -0700

    6/38 Constructor Method

--- src/Doctor.java
+++ src/Doctor.java
@@ -4,6 +4,19 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor(){
+        System.out.println("Building the Doctor object");
+    }
+
+    //Function overloading
+    /**
+     * Description: Build a new object of doctor class
+     * @name Name to be assigned to the doctor object
+     * **/
+    Doctor(String name){
+        System.out.println("The name assigned to the doctor object is: "+ name);
+    }
+
     //Methods
     /**
      * Description: Print the name of the doctor.
