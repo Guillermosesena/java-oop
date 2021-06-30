ce4b270acbf85ab7ff0f1d99987d5faa6737d032
Guillermo Sesena <g.sesena.pascacio@accenture.com>
2021-06-29 17:06:17 -0700

    9/38 Constant in Java

--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -2,6 +2,8 @@ package ui;
 
 import java.util.Scanner;
 public class UIMenu {
+    public static String[] MONTHS = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
     public static void showMenu() {
         System.out.println("Welcome to My Appointments");
         System.out.println("Select your option");
@@ -50,6 +52,9 @@ public class UIMenu {
             switch (response) {
                 case 1:
                     System.out.println("::Book an appointment");
+                    for (int i = 0; i < 4; i++) {
+                        System.out.println(i + ". " + MONTHS[i]);
+                    }
                     break;
                 case 2:
                     System.out.println("::My appointments");
