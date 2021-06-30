2cd75d37276db5c4e0a1b474ae4509719bd8d010
Guillermo Sesena <g.sesena.pascacio@accenture.com>
2021-06-29 21:12:42 -0700

    18/38 Enumerations

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor("Anah Salgado", "dentist");
+        /*Doctor myDoctor = new Doctor("Anah Salgado", "dentist");
         myDoctor.addAvailableAppointment(new Date(),"4 pm");
         myDoctor.addAvailableAppointment(new Date(),"10 pm");
         myDoctor.addAvailableAppointment(new Date(),"1 pm");
@@ -14,6 +14,15 @@ public class Main {
 
         for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
             System.out.println(aA.getDate() + " " +aA.getTime());
+        }*/
+        Day allDays[] = Day.values();
+
+        for (Day d: allDays) {
+            System.out.println(d);
         }
+        
+        System.out.println(Day.MONDAY);
+        System.out.println(Day.MONDAY.getSpanish());
+
     }
 }
